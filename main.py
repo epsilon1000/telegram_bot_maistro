59c014fbb4f90129a2c8d603ec2818f870caf5a6
epsilon1000 <edtejada77@gmail.com>
2023-05-24 16:50:59 -0500

    Primer commit, versión básica del bot

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import openai
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+bot = Bot(token=os.getenv("TELEGRAM_BOT_KEY"))
+
+dp = Dispatcher(bot)
+
+#first_prompt = 'You are an useful assistant called "Maistro", you are programmed to answer any questions the user asks'
+
+
+
+@dp.message_handler(commands = ['start', 'help'])
+async def welcome(message: types.Message):
+    await message.reply('¡Hola! Soy el Maistro, un bot que sabe cosas. ¿En qué puedo ayudarte?')
+
+
+@dp.message_handler()
+async def gpt(message: types.Message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message.text,
+        temperature=0.5,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    await message.reply(response.choices[0].text)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
